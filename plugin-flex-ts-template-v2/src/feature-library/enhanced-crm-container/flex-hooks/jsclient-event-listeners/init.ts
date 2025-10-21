9e273fd6595d0cea81520edcdc0cd4351c5fca5b
Welder Canesin <>
2025-10-21 10:05:47 -0300

    Remove invalid init.ts file causing TypeScript error

--- plugin-flex-ts-template-v2/src/feature-library/enhanced-crm-container/flex-hooks/jsclient-event-listeners/init.ts
+++
@@ -1,9 +0,0 @@
-import { FlexJsClient } from '../../../../types/feature-loader';
-import { registerLoadCRMContainerTabsAction } from '../custom-action/loadCRMContainerTabs';
-
-export const clientName = FlexJsClient.FlexClient;
-export const eventName = 'init';
-export const jsClientHook = function registerCRMActionEarly() {
-  console.log('[enhanced-crm-container] Registering LoadCRMContainerTabs action during init');
-  registerLoadCRMContainerTabsAction();
-};
